8023b93c14d06886bd1d52a426ec27258ceb7f5f
tylerswan11 <tylerswan11@gmail.com>
2016-05-16 19:22:20 -0700

    added led widget to be added dynamically
    added led widget to be added dynamically

--- Dashboard Application/src/dashboard/application/LEDWidget.java
+++ Dashboard Application/src/dashboard/application/LEDWidget.java
@@ -5,16 +5,36 @@
  */
 package dashboard.application;
 
+import javafx.geometry.Pos;
+import static javafx.scene.Node.BASELINE_OFFSET_SAME_AS_HEIGHT;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+
 /**
  *
  * @author Brennan
  */
 public class LEDWidget extends DataWidget {
 
-    // Fields
-    public String LastDisplayedValue;
+    final HBox hb = null;
 
     public LEDWidget() {
-     
+
+            // New Label object
+            final Label label = new Label();
+            label.setText("LED:" );
+
+            final Circle led = new Circle();
+            final HBox hb = new HBox();
+            hb.setAlignment(Pos.CENTER);
+            hb.getChildren().addAll(label, led);
+            
+            this.getChildren().add(hb);
+    }
+
+    public static void main(String[] args) {
+        DataWidget.launch(args);
     }
 }
