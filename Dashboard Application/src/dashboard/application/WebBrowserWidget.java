30131b63a66ebe8af6daad6d355211dfb3e1100c
Tanner Mickelson <tanner@icicle-games.com>
2016-05-31 19:58:30 -0700

    Added property editing to web browser widget

--- Dashboard Application/src/dashboard/application/WebBrowserWidget.java
+++ Dashboard Application/src/dashboard/application/WebBrowserWidget.java
@@ -5,6 +5,12 @@
  */
 package dashboard.application;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -25,14 +31,44 @@ public class WebBrowserWidget extends DataWidget {
     final WebEngine webEngine = browser.getEngine();
     final HBox hb = new HBox();
     
+    public StringProperty url = new SimpleStringProperty(this, "URL", "http://www.arma2base.de/content/images/ac130_12.jpg");
     
-    public WebBrowserWidget() {
+    public DoubleProperty width = new SimpleDoubleProperty(this, "width", 1);
+    public DoubleProperty height = new SimpleDoubleProperty(this, "height", 1);
+    
+    public WebBrowserWidget()
+    {
+        url.addListener((e) -> this.urlChanged());
+        url.addListener((e) -> this.widthChanged());
+        url.addListener((e) -> this.heightChanged());
         
-        webEngine.load("https://docs.oracle.com/javase/8/javafx/embedded-browser-tutorial/overview.htm");
+        webEngine.load(url.get());
         browser.setLayoutX(100.0);
+        
+        browser.setScaleX(width.get());
+        browser.setScaleY(height.get());
         
         hb.setAlignment(Pos.CENTER_RIGHT);
         
         this.getChildren().add(browser);
+        
+        addEditableProperty(width);
+        addEditableProperty(height);
+        addEditableProperty(url);
+    }
+    
+    private void urlChanged()
+    {
+        webEngine.load(url.get());
+    }
+    
+    private void widthChanged()
+    {
+        browser.setScaleX(width.get());
+    }
+    
+    private void heightChanged()
+    {
+        browser.setScaleY(height.get());
     }
 }
